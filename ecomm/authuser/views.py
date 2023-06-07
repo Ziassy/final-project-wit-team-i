0523b68fd670a1d29d8ccda07bb5a2cf2d322528
ziassy <fauziahfz216@gmail.com>
2023-06-07 22:40:23 +0700

    [HIDE] Forgot password and delete class ActivateAccountView(View): and email activation

--- ecomm/authuser/views.py
+++ ecomm/authuser/views.py
@@ -35,38 +35,9 @@ def signup(request):
         user = User.objects.create_user(username,email,password)
         user.is_active=True #yang baru signi[, isactive = false
         user.save()
-        # email_subject="Activate Your Account"
-        # message=render_to_string('account/activate.html',{
-        #      'user':user,
-        #      'domain':'127.0.0.1:8000',
-        #      'uid':urlsafe_base64_encode(force_bytes(user.pk)), #urlsafe_base64_encode generate token
-        #      'token':generate_token.make_token(user)
-
-        #  })
-        # user = user
-        # domain = '127.0.0.1:8000'
-        # uid = urlsafe_base64_encode(force_bytes(user.pk))
-        # token = generate_token.make_token(user)
-        #https://www.youtube.com/watch?v=7VEveJz7hdM 4:01:42
-        # email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
-        # email_message.send()
-        # messages.success(request,f"Activate Your Account by clicking this link http://{domain}/'activate'/{uid}/{token}")
         return redirect('/auth/login/')
     return render(request,"account/signup.html")
 
-# class ActivateAccountView(View):
-#     def get(self,request,uidb64,token):
-#         try:
-#             uid=force_str(urlsafe_base64_decode(uidb64))
-#             user=User.objects.get(pk=uid)
-#         except Exception as identifier:
-#             user=None
-#         if user is not None and generate_token.check_token(user,token):
-#             user.is_active=True
-#             user.save()
-#             messages.info(request,"Account Activated Successfully")
-#             return redirect('/auth/login')
-#         return render(request,'account/activatefail.html') #jika gagal redirect ke activate fail
 
 @csrf_protect
 def handlelogin(request):
